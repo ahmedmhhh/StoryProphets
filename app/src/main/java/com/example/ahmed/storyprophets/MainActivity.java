06b3fdfbb1d928b4736e94dffc7ef5ff160e41fd
Ahmed M.Hassan <a7medmhhh@hotmail.com>
2018-06-19 06:15:30 +0200

    -send email , exit , show source code and go to my github

--- app/src/main/java/com/example/ahmed/storyprophets/MainActivity.java
+++ app/src/main/java/com/example/ahmed/storyprophets/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.ahmed.storyprophets;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +32,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void img_share(View view) {
+        String txtShare = "قصص الانبياء";
+        String link = "https://github.com/ahmedmhhh/StoryProphets";
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(intent.EXTRA_TEXT,txtShare+"\n"+link);
+        startActivity(intent);
+    }
+
+    public void img_more(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://github.com/ahmedmhhh"));
+        startActivity(intent);
+    }
+
+    public void img_email(View view) {
+        try {
+            String txt = "Hi Ahmed !";
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("message/rfc822");
+            i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"a7medmhhh@hotmail.com"});
+            i.putExtra(Intent.EXTRA_SUBJECT, "قصص الانبياء");
+            i.putExtra(Intent.EXTRA_TEXT   , txt);
+            try {
+                startActivity(Intent.createChooser(i, "Send mail..."));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(MainActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+            }
+        }catch (Exception e){
+            Toast.makeText(this,"don't have mail application",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void img_exit(View view) {
+        this.finish();
     }
 }
